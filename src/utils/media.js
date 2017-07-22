a392bb9b24f90e99dab3a4779d9966aebd789c40
Andreas Reiterer <reiterer.a@gmail.com>
2017-07-22 16:10:06 +0200

    adapted media helpers for mobile first usage

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,13 +1,19 @@
 import { css } from "styled-components";
 
-export const mobile = inner => css`
-  @media(max-width: ${1000 / 16}em) {
+export const phone = inner => css`
+  @media only screen and (min-width : ${480 / 16}em) {
     ${inner}
   }
 `;
 
-export const phone = inner => css`
-  @media(max-width: ${650 / 16}em) {
+export const tablet = inner => css`
+  @media only screen and (min-width : ${768 / 16}em) {
+    ${inner}
+  }
+`;
+
+export const desktop = inner => css`
+  @media only screen and (min-width : ${992 / 16}em) {
     ${inner}
   }
 `;
