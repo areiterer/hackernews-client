da319e139c240e8421445e79f5505069e956760c
Andreas Reiterer <reiterer.a@gmail.com>
2017-07-24 20:04:11 +0200

    Add loading spinner to StoryList

--- src/containers/StoryList.js
+++ src/containers/StoryList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Spinner from "react-spinkit";
 
 import Story from "../components/Story";
 
 const Wrapper = styled.div`margin: 20px;`;
+const LoadingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 50px;
+`;
 
 export default class StoryList extends Component {
   render() {
@@ -12,7 +19,12 @@ export default class StoryList extends Component {
 
     if (items.length > 0) {
       return this.renderList(items);
-    } else return <div>Loading...</div>;
+    } else
+      return (
+        <LoadingWrapper>
+          <Spinner name="ball-grid-pulse" fadeIn="none" />
+        </LoadingWrapper>
+      );
   }
 
   renderList(items) {
